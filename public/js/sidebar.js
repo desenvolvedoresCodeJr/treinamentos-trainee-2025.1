35e00aab9a014e8985eb6d9d452ad61b204ca68a
ViniCampos12 <viniciuspriamo@gmail.com>
2025-04-10 16:28:29 -0300

    Código finalizado

---
+++ public/js/sidebar.js
@@ -0,0 +1,6 @@
+const buttonToggle = document.querySelector('.toggle-sidebar');
+
+buttonToggle.addEventListener('click', () => {
+    document.querySelector('.sidebar').classList.toggle('closed-sidebar');
+
+})
