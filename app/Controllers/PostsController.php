bc940e58eb103b6a89c478f6989bbd482eba8b96
lucasferoli <lucasferoliveira.goval@gmail.com>
2025-04-27 21:01:40 -0300

    Adiciona funcionalidade aos botoes de editar e deletar no adm post

--- app/Controllers/PostsController.php
+++ app/Controllers/PostsController.php
@@ -60,6 +60,7 @@ class PostsController
             'titulo' => $_POST['titulo'],
             'descricao' => $_POST['descricao'],
             'imagem' =>  $caminhodaimagem,
+
             'id_autor' => 1,
 
         ];
@@ -71,27 +72,60 @@ class PostsController
 
     public function edit()
     {
+        $id = $_POST['id'];
+        $post = App::get('database')->selectAllWithSearch('posts', 'id', $id);
+        
+        if (count($post) > 0) {
+            $post = $post[0];
+            $caminhoImagem = $post->imagem;
+    
+            if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
+                $temporario = $_FILES['imagem']['tmp_name'];
+                $nomeimagem = sha1(uniqid($_FILES['imagem']['name'], true)) . '.' . pathinfo($_FILES['imagem']['name'], PATHINFO_EXTENSION);
+                $destinoimagem = "public/assets/imagensPosts/";
+                move_uploaded_file($temporario, $destinoimagem . $nomeimagem);
+                $caminhoImagem = "public\\assets\\imagensPosts\\" . $nomeimagem;
+    
+                if (file_exists($post->imagem)) {
+                    unlink($post->imagem);
+                }
+            }
+        }
+    
         $parameters = [
             'titulo' => $_POST['titulo'],
             'descricao' => $_POST['descricao'],
-            'imagem' => $_POST['imagem'],
+            'imagem' => $caminhoImagem,
             'criado_em' => $_POST['criado_em'],
             'id_autor' => $_POST['id_autor'],
         ];
-
-        $id = $_POST['id'];
-
+    
         App::get('database')->update('posts', $id, $parameters);
-
         header('Location: /crudPosts');
     }
     
     public function delete()
     {
         $id = $_POST['id'];
+    
+        $posts = App::get('database')->selectAllWithSearch('posts', 'id', $id);
+        if(count($posts) >0){
+            $post = $posts[0];
+        }
+        
+        if(isset($post->imagem)){
+            $caminhodaimagem = $post->imagem;
+        }
+        
+        if(file_exists($caminhodaimagem)){
+            unlink($caminhodaimagem);
+        }
+
         App::get('database')->delete('posts', $id);
 
+
         header('Location: /crudPosts');
+
     }
 }
 
